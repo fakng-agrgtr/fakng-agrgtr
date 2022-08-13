1997d24e97f574dd73bb07ab03c9051895d72dc9
Maks Vinnytskyi <ownerofforest@gmail.com>
2022-08-13 17:08:26 +0300

    Implement GoogleParser. Add GSON dependency.

---
+++ src/main/java/com/fakng/fakngagrgtr/parser/impl/google/GoogleParser.java
@@ -0,0 +1,84 @@
+package com.fakng.fakngagrgtr.parser.impl.google;
+
+import com.fakng.fakngagrgtr.entity.Vacancy;
+import com.fakng.fakngagrgtr.parser.ApiParser;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GoogleParser extends ApiParser {
+
+    public GoogleParser(@Autowired WebClient webClient, @Value("${url.google}") String url) {
+        super(webClient);
+        this.url = url;
+    }
+
+    @Override
+    protected List<Vacancy> process() throws Exception {
+        ResponseDTO firstPage = getPage(1);
+        List<VacancyDTO> googleVacancies = new ArrayList<>(firstPage.jobs);
+        for (int i = 2; i <= firstPage.count / firstPage.page_size + 1; i += 1) {
+            System.out.println("Page " + i);
+            ResponseDTO page = getPage(i);
+            googleVacancies.addAll(page.jobs);
+        }
+        return googleVacancies.stream().map(this::createVacancy).toList();
+    }
+
+    private Vacancy createVacancy(VacancyDTO dto) {
+        Vacancy vacancy = new Vacancy();
+        vacancy.setId(Long.parseLong(dto.id.split("/")[1]));
+        vacancy.setTitle(dto.title);
+        vacancy.setUrl(dto.applyUrl);
+        vacancy.setCompany(
+                null); // Google? dto.companyName? dto.companyId? It may be YouTube or anything else.
+        if (dto.publishDate != null) {
+            vacancy.setAddDate(LocalDateTime.parse(dto.publishDate,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX")));
+        }
+        vacancy.setLocation(null); // dto.locations?
+        vacancy.setDescription(
+                dto.description + "\n" +
+                dto.summary + "\n" +
+                dto.qualifications + "\n" +
+                dto.responsibilities + "\n" +
+                dto.additionalInstructions
+        );
+        return vacancy;
+    }
+
+    private ResponseDTO getPage(int index) throws IOException {
+        String body = sendRequest(url + "&page=" + index);
+        return new Gson().fromJson(body, ResponseDTO.class);
+    }
+
+    private String sendRequest(String url) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
+
+        conn.getResponseCode(); // send request
+        StringBuilder responseContent = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            responseContent.append(line);
+        }
+        reader.close();
+        return responseContent.toString();
+    }
+}
---
+++ src/main/java/com/fakng/fakngagrgtr/parser/impl/google/ResponseDTO.java
@@ -0,0 +1,11 @@
+package com.fakng.fakngagrgtr.parser.impl.google;
+
+import java.util.List;
+
+public class ResponseDTO {
+
+    public Integer count;
+    public Integer next_page;
+    public Integer page_size;
+    public List<VacancyDTO> jobs;
+}
