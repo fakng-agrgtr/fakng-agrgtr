fb2adf4da3a919984c768943a617b8a3b7657418
Maks Vinnytskyi <ownerofforest@gmail.com>
2022-08-13 16:11:59 +0300

    Describe company model from Google API.

---
+++ src/main/java/com/fakng/fakngagrgtr/parser/impl/google/CompanyDTO.java
@@ -0,0 +1,30 @@
+package com.fakng.fakngagrgtr.parser.impl.google;
+
+import java.util.List;
+import java.util.Map;
+
+public class CompanyDTO {
+
+    private String id;
+    private String title;
+    private List<String> categories;
+    private String apply_url;
+    private String responsibilities;
+    private String qualifications;
+    private String company_id;
+    private String company_name;
+    private String language_code;
+    private List<Map<String, Object>> locations;
+    private String description;
+    private List<String> education_levels;
+    private String created;
+    private String modified;
+    private String publish_date;
+    private String application_instruction; // ?
+    private String eeo;
+    private Integer locations_count;
+    private String additional_instructions;
+    private String summary;
+    private List<Map<String, Integer>> building_pins;
+    private Boolean has_remote;
+}
