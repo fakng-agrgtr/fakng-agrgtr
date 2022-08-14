1e66ec60cbcc59bc06282fc0f555abe219822e41
mighty-mhsl <mihusle@protonmail.com>
2022-08-14 18:59:37 +0400

    Small refactoring

--- src/main/java/com/fakng/fakngagrgtr/parser/HtmlParser.java
+++ src/main/java/com/fakng/fakngagrgtr/parser/HtmlParser.java
@@ -3,14 +3,12 @@ package com.fakng.fakngagrgtr.parser;
 import com.fakng.fakngagrgtr.company.CompanyRepository;
 import com.fakng.fakngagrgtr.location.LocationRepository;
 import com.fakng.fakngagrgtr.parser.cache.LocationCache;
-import com.fakng.fakngagrgtr.vacancy.Vacancy;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
 @Slf4j
@@ -22,17 +20,6 @@ public abstract class HtmlParser extends Parser {
                       CompanyRepository companyRepository, LocationRepository locationRepository) {
         super(locationCache, companyRepository, locationRepository);
         this.htmlWebClient = htmlWebClient;
-    }
-
-    @Override
-    public List<Vacancy> getAllVacancies() {
-        try {
-            HtmlPage page = downloadPage();
-            return process(page);
-        } catch (Exception e) {
-            log.error("Error while parsing page under url: {}", url, e);
-        }
-        return null; //хз
     }
 
     private HtmlPage downloadPage(String url) throws IOException {
