a3075f7ea6dfe75fd70a4d651aa46ed5ec89cbd5
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-08-15 13:11:52 +0300

    changed

--- src/main/java/com/fakng/fakngagrgtr/controller/CustomControllerAdvice.java
+++ src/main/java/com/fakng/fakngagrgtr/controller/CustomControllerAdvice.java
@@ -10,64 +10,40 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.xml.bind.ValidationException;
 
-/**
- * Централизированное управление ошибками.<br>
- * Все ошибки летят сюда.
- *
- * В любой момент можем добавить другой метод, который ловит определенный тип ошибок
- */
 @Slf4j
 @RestControllerAdvice
 class CustomControllerAdvice {
 
-    /**
-     * Если летит наша ошибка
-     * @param e выброшенный exception
-     * @return ответ с ошибкой 400
-     */
+
     @ExceptionHandler(FakngException.class)
     public ResponseEntity<String> handleLibraryExceptions(FakngException e) {
-        log.error(e.getClass().toString());
-        log.error(e.getMessage());
-        for (StackTraceElement element : e.getStackTrace())
-            log.error(element.toString());
+        logStackTrace(e);
         return new ResponseEntity<>(
                 e.getMessage(),
                 HttpStatus.valueOf(400)
         );
     }
 
-    /**
-     * Всякие ошибки валидации и т д
-     * @param e выброшенный exception
-     * @return ответ с ошибкой 400
-     */
     @ExceptionHandler({ValidationException.class, HttpMessageNotReadableException.class})
     public ResponseEntity<String> handleJsonExceptions(Exception e) {
-        log.error(e.getClass().toString());
-        log.error(e.getMessage());
-        for (StackTraceElement element : e.getStackTrace())
-            log.error(element.toString());
+        logStackTrace(e);
         return new ResponseEntity<>(
                 FakngError.VALIDATION_ERROR.toString(),
                 HttpStatus.valueOf(400)
         );
     }
 
-    /**
-     * Все другие ошибки
-     * @param e выброшенный exception (не наш)
-     * @return ответ с ошибкой 500(тут скорее уже ошибка нашего сервера)
-     */
     @ExceptionHandler
     public ResponseEntity<String> handleOtherExceptions(Exception e) {
-        log.error(e.getClass().toString());
-        log.error(e.getMessage());
-        for (StackTraceElement element : e.getStackTrace())
-            log.error(element.toString());
+        logStackTrace(e);
         return new ResponseEntity<>(
                 FakngError.TECHNICAL_ERROR.toString(),
                 HttpStatus.valueOf(500)
         );
+    }
+
+    private void logStackTrace(Exception e) {
+        for (StackTraceElement element : e.getStackTrace())
+            log.error(element.toString());
     }
 }
