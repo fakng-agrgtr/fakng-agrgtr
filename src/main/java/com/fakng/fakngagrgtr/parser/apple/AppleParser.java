77b2cba2c94b41715e567e8521e2646de8c812fb
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-08-16 20:10:40 +0300

    fix

--- src/main/java/com/fakng/fakngagrgtr/parser/apple/AppleParser.java
+++ src/main/java/com/fakng/fakngagrgtr/parser/apple/AppleParser.java
@@ -1,12 +1,13 @@
 package com.fakng.fakngagrgtr.parser.apple;
 
-import com.fakng.fakngagrgtr.parser.ApiParser;
+import com.fakng.fakngagrgtr.parser.HtmlParser;
 import com.fakng.fakngagrgtr.parser.LocationProcessor;
 import com.fakng.fakngagrgtr.persistent.company.CompanyRepository;
 import com.fakng.fakngagrgtr.persistent.location.Location;
 import com.fakng.fakngagrgtr.persistent.vacancy.Vacancy;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gargoylesoftware.htmlunit.WebClient;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
@@ -15,30 +16,31 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class AppleParser extends ApiParser {
+public class AppleParser extends HtmlParser {
 
     private static final int TIMEOUT_REQUEST = 5000;
     private static final String URL_FOR_VACANCY = "https://jobs.apple.com/en-us/details/";
     private static final String COUNT_PAGES_ADDRESS = "span[class=pageNumber]";
     private static final String JSON_BODY_ADDRESS = "script[type=text/javascript]";
 
+    private static final JSONParser jsonParser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+    private static final ObjectMapper mapper = new ObjectMapper();
 
-    public AppleParser(WebClient webClient,
+
+    public AppleParser(WebClient htmlWebClient,
                        CompanyRepository companyRepository,
                        LocationProcessor locationProcessor,
                        @Value("${url.apple}") String url) {
-        super(webClient, companyRepository, locationProcessor);
+        super(htmlWebClient, companyRepository, locationProcessor);
         this.url = url;
     }
 
@@ -55,29 +57,29 @@ public class AppleParser extends ApiParser {
     @Override
     protected List<Vacancy> getAllVacancies() throws Exception {
         Document doc = getResponseByPage(0);
-        int countPages = takeCountPages(doc);
-        List<Vacancy> allVacancies = new ArrayList<>(takeVacanciesFromDoc(doc));
-        for (int i = 1; i < 2; i++) {
+        int countPages = getCountPages(doc);
+        List<Vacancy> allVacancies = new ArrayList<>(getVacanciesFromDoc(doc));
+        for (int i = 1; i < countPages; i++) {
             doc = getResponseByPage(i);
-            allVacancies.addAll(takeVacanciesFromDoc(doc));
+            allVacancies.addAll(getVacanciesFromDoc(doc));
         }
         return allVacancies;
     }
 
-    private Document getResponseByPage(int x) throws IOException {
-        return Jsoup.parse(new URL(String.format(url, x)), TIMEOUT_REQUEST);
+    private Document getResponseByPage(int numberOfPage) throws IOException {
+        return Jsoup.parse(new URL(String.format(url, numberOfPage)), TIMEOUT_REQUEST);
     }
 
-    private int takeCountPages(Document doc) {
+    private int getCountPages(Document doc) {
         return Integer.parseInt(doc.select(COUNT_PAGES_ADDRESS).get(1).text());
     }
 
-    private List<Vacancy> takeVacanciesFromDoc(Document doc) throws ParseException {
-        String jsonBody = takeJsonBody(doc);
-        return takeVacanciesFromJsonBody(jsonBody);
+    private List<Vacancy> getVacanciesFromDoc(Document doc) throws ParseException, JsonProcessingException {
+        String jsonBody = getJsonBody(doc);
+        return getVacanciesFromJsonBody(jsonBody);
     }
 
-    private String takeJsonBody(Document doc) {
+    private String getJsonBody(Document doc) {
         return doc.select(JSON_BODY_ADDRESS)
                 .first()
                 .childNode(0).toString()
@@ -86,28 +88,22 @@ public class AppleParser extends ApiParser {
                 .substring(19);
     }
 
-    private List<Vacancy> takeVacanciesFromJsonBody(String json) throws ParseException {
+    private List<Vacancy> getVacanciesFromJsonBody(String json) throws ParseException, JsonProcessingException {
         JSONObject jsonBody = parse(json);
         JSONArray searchResults = (JSONArray) jsonBody.get("searchResults");
         List<Vacancy> vacancies = new ArrayList<>();
         for (Object vacancyJson : searchResults) {
-            try {
-                VacancyApple vacancyApple = toVacancyApple(vacancyJson);
-                vacancies.add(toVacancy(vacancyApple));
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
+            VacancyApple vacancyApple = toVacancyApple(vacancyJson);
+            vacancies.add(toVacancy(vacancyApple));
         }
         return vacancies;
     }
 
     private JSONObject parse(String json) throws ParseException {
-        JSONParser jsonParser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
-        return  (JSONObject) jsonParser.parse(json);
+        return (JSONObject) jsonParser.parse(json);
     }
 
     private VacancyApple toVacancyApple(Object vacancyJson) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(vacancyJson.toString(), VacancyApple.class);
     }
 
@@ -116,8 +112,7 @@ public class AppleParser extends ApiParser {
         vacancy.setTitle(vacancyApple.getPostingTitle());
         vacancy.setDescription(generateFullDescription(vacancyApple));
         vacancy.setUrl(URL_FOR_VACANCY + vacancyApple.getPositionId());
-        vacancy.setAddDate(LocalDateTime.now());
-        vacancy.setPublishedDate(parseVacancyAppleDateTime(vacancyApple.getPostDateInGMT()));
+        vacancy.setPublishedDate(vacancyApple.getPostDateTime());
         vacancy.setCompany(company);
         vacancy.setLocations(toLocations(vacancyApple.getLocations()));
         return vacancy;
@@ -129,10 +124,6 @@ public class AppleParser extends ApiParser {
                 vacancyApple.getJobSummary();
     }
 
-    private LocalDateTime parseVacancyAppleDateTime(String dateTime) {
-        return LocalDateTime.parse(dateTime.replace("Z", ""));
-    }
-
     private List<Location> toLocations(List<LocationApple> locationsApple) {
         return locationsApple.stream()
                 .map(location -> locationProcessor.processLocation(company, location.getCity(), parseCountry(location.getCountryID())))
@@ -140,6 +131,6 @@ public class AppleParser extends ApiParser {
     }
 
     private String parseCountry(String country) {
-        return country.split("-")[1];
+        return country.substring(country.indexOf('-'));
     }
 }
--- src/main/java/com/fakng/fakngagrgtr/parser/apple/LocationApple.java
+++ src/main/java/com/fakng/fakngagrgtr/parser/apple/LocationApple.java
@@ -1,35 +1,24 @@
 package com.fakng.fakngagrgtr.parser.apple;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class LocationApple {
-    @JsonProperty("city")
+
     public String city;
 
-    @JsonProperty("level")
     public Integer level;
 
-    @JsonProperty("metro")
     public String metro;
 
-    @JsonProperty("name")
     public String name;
 
-    @JsonProperty("stateProvince")
     public String stateProvince;
 
-    @JsonProperty("countryName")
     public String countryName;
 
-    @JsonProperty("region")
     public String region;
 
-    @JsonProperty("countryID")
     public String countryID;
 
 }
