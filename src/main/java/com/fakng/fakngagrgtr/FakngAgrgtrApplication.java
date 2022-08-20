7a5f8c93f750cf7b95da17bed5410fb933bd89e8
mighty-mhsl <mihusle@protonmail.com>
2022-08-20 12:16:13 +0400

    Added Uber parser unit tests

--- src/main/java/com/fakng/fakngagrgtr/FakngAgrgtrApplication.java
+++ src/main/java/com/fakng/fakngagrgtr/FakngAgrgtrApplication.java
@@ -1,25 +1,14 @@
 package com.fakng.fakngagrgtr;
 
-import com.fakng.fakngagrgtr.parser.uber.UberParser;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@RequiredArgsConstructor
-public class FakngAgrgtrApplication implements CommandLineRunner {
-
-    private final UberParser uberParser;
+public class FakngAgrgtrApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FakngAgrgtrApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        uberParser.parse();
     }
 }
