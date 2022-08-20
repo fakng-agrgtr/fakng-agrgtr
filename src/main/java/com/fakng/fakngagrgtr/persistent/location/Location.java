e866c64d65e0f11d5a42d832428f4cda63f212fb
mighty-mhsl <mihusle@protonmail.com>
2022-08-20 18:34:24 +0400

    Enrich apple vacancies with additional data from every page separately

--- src/main/java/com/fakng/fakngagrgtr/persistent/location/Location.java
+++ src/main/java/com/fakng/fakngagrgtr/persistent/location/Location.java
@@ -7,7 +7,14 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,9 +46,5 @@ public class Location {
 
     public void addCompany(Company company) {
         companies.add(company);
-    }
-
-    public void addVacancy(Vacancy vacancy) {
-        vacancies.add(vacancy);
     }
 }
--- src/main/java/com/fakng/fakngagrgtr/persistent/vacancy/Vacancy.java
+++ src/main/java/com/fakng/fakngagrgtr/persistent/vacancy/Vacancy.java
@@ -7,7 +7,16 @@ import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -56,6 +65,5 @@ public class Vacancy {
 
     public void addLocation(Location location) {
         locations.add(location);
-        location.addVacancy(this);
     }
 }
